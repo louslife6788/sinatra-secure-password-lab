8ed1cf04647f6de664979ff026593138c78176fe
luis leon <leonluis380@yahoo.com>
2022-01-20 12:05:12 -0500

    commit tree e69b7b30c02905ef43c7437f5f41c08814896d54

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-    user = User.new( username: params[:username],password: params[:password])
-   
-    if user.save
-			redirect "/login"
-		  else
-			redirect "/failure"
-		  end
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    end
 
   end
 
@@ -39,19 +37,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
     @user = User.find_by(username: params[:username])
-    
     if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect to "/account" 
-    else 
-        redirect to "/failure"
-      
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else
+      redirect to "/failure"
     end
-
-
-
   end
 
   get "/failure" do
