328bd2678b66c9e992e270a4b1dac305f52ad4ac
luis leon <leonluis380@yahoo.com>
2022-01-20 11:54:43 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,8 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    user = User.new( username: params[:username],password_digest: params[:password])
+    user = User.new( username: params[:username],password: params[:password])
+   
     if user.save
 			redirect "/login"
 		  else
@@ -39,7 +40,8 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
-    @user = User.find_by(params[:username])
+    @user = User.find_by(username: params[:username])
+    
     if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
         redirect to "/account" 
